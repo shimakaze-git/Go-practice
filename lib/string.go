d781bd32e711c33de1aa53537854102e21a7ed85
shimakaze-git <shimakaze.soft+github@googlemail.com>
2017-07-28 08:17:01 +0000

    main関数追加

---
+++ lib/string.go
@@ -0,0 +1,3 @@
+package message
+
+var Message string = "hello Message"
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"Go-practice/lib"
+	"fmt"
+)
+
+func main() {
+	fmt.Println(message.Message) // hello world
+}
