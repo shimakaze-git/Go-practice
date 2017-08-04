25f3c74a079965124460dd77268be1f9b8488e92
shimakaze-git <shimakaze.soft+github@googlemail.com>
2017-08-04 14:43:12 +0000

    一度に複数の宣言と初期化

--- practice/convert_toUpper.go
+++ practice/convert_toUpper.go
@@ -6,8 +6,15 @@ import (
 )
 
 func main() {
-	str := "GoLang"
+
+	var (
+		str      string = "GoLang"
+		java_str        = "java"
+		c_str           = "c"
+	)
 
 	//全て大文字に変換する
 	fmt.Println(strings.ToUpper(str))
+	fmt.Println(strings.ToUpper(java_str))
+	fmt.Println(strings.ToUpper(c_str))
 }
