d043b79e21286373f02af9aa316b737925573b83
shimakaze-git <shimakaze.soft+github@googlemail.com>
2017-08-05 15:00:47 +0000

    小文字に変換する処理を追加

--- practice/convert_toUpper.go
+++ practice/convert_toUpper.go
@@ -9,12 +9,17 @@ func main() {
 
 	var (
 		str      string = "GoLang"
-		java_str        = "java"
-		c_str           = "c"
+		java_str        = "Java"
+		c_str           = "C"
 	)
 
 	//全て大文字に変換する
 	fmt.Println(strings.ToUpper(str))
 	fmt.Println(strings.ToUpper(java_str))
 	fmt.Println(strings.ToUpper(c_str))
+
+	//全て小文字に変換する
+	fmt.Println(strings.ToLower(str))
+	fmt.Println(strings.ToLower(java_str))
+	fmt.Println(strings.ToLower(c_str))
 }
